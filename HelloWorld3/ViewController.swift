1445ac173458ba7708a52ae80fd2887bf8ac1b8d
Морозов Евгений <evgenii.morozov@sparm.com>
2025-02-01 23:20:06 +0300

    test

--- HelloWorld3/ViewController.swift
+++ HelloWorld3/ViewController.swift
@@ -24,32 +24,37 @@ final class ViewController: UIViewController {
         super.viewDidLoad()
         setupMainView()
         setupSlider()
+        setColorView()
     }
     
     // MARK: - IB Actions
     @IBAction private func redSliderAction() {
         howMuchRedLabel.text = (round(redSlider.value * 100) / 100).formatted()
+        setColorView()
     }
     
     @IBAction private func greenSliderAction() {
         howMuchGreenLabel.text = (round(greenSlider.value * 100) / 100).formatted()
+        setColorView()
     }
     
     @IBAction private func blueSliderAction() {
         howMuchBlueLabel.text = (round(blueSlider.value * 100) / 100).formatted()
+        setColorView()
     }
 }
 
 // MARK: - Setup UI
 private extension ViewController {
     func setupMainView() {
+        screenView.layer.cornerRadius = 15
+    }
+    
+    func setColorView() {
         let red: CGFloat = CGFloat(redSlider.value)
         let green: CGFloat = CGFloat(greenSlider.value)
         let blue: CGFloat = CGFloat(blueSlider.value)
         screenView.backgroundColor = UIColor(red: red, green: green, blue: blue, alpha: 1)
-        screenView.layer.cornerRadius = 15
-        
-//        screenView.backgroundColor = UIColor(red: CGFloat, green: <#T##CGFloat#>, blue: <#T##CGFloat#>, alpha: 1)
     }
     
     func setupSlider() {
